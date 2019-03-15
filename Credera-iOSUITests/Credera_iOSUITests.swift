3bc7d24ea19e5fbda73146ec6841296a19aedf07
Evan Kirkland <ekirkland@credera.com>
2019-03-14 19:22:42 -0500

    Adding UI tests for navigation.

--- Credera-iOSUITests/Credera_iOSUITests.swift
+++ Credera-iOSUITests/Credera_iOSUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class Credera_iOSUITests: XCTestCase {
+    var app: XCUIApplication!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,15 +21,35 @@ class Credera_iOSUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication.init()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testVCNavigation() {
+        // Ensure navigation via buttons is working properly
+        XCTAssert(app.staticTexts["Navigation has not been completed"].exists)
+        app.buttons["Navigation Example"].tap()
+        XCTAssert(app.staticTexts["This is an example of passing information from one VC to another"].exists)
+        app.buttons["Navigate to final screen"].tap()
+        XCTAssert(app.staticTexts["This is an example of passing information from one VC to another"].exists)
+        app.buttons["Navigate back to First VC"].tap()
+        XCTAssert(app.staticTexts["Navigation to Final VC has been completed"].exists)
+    }
+    
+    func testTabBarNavigation() {
+        // Ensure navigation via toolbar is working properly
+        XCTAssert(app.staticTexts["MainTabBarApp: FirstViewController"].exists)
+        
+        // No change; already on first screen
+        app.tabBars.buttons["First"].tap()
+        XCTAssert(app.staticTexts["MainTabBarApp: FirstViewController"].exists)
+        
+        // Navigate to second tab
+        app.tabBars.buttons["Second"].tap()
+        XCTAssert(app.staticTexts["MainTabBarApp: SecondViewController"].exists)
     }
 
 }
